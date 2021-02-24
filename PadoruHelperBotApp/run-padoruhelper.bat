991623ee02112640fd1352adee3efccc44eacbe4
Santiago Gerhart <gerhartsantiago@gmail.com>
2021-02-24 17:05:44 -0300

    Delete test files

--- PadoruHelperBotApp/run-padoruhelper.bat
+++
@@ -1,4 +0,0 @@
-SET ASPNETCORE_ENVIRONMENT=Development
-SET LAUNCHER_PATH=bin\Debug\netcoreapp3.1\PadoruHelperBotApp.exe
-cd /d "C:\Program Files\IIS Express\"
-iisexpress.exe /config:"D:\Santi\Documents\GitHub\padoruhelper-bot\.vs\PadoruHelperBot\config\applicationhost.config" /site:"PadoruHelperBotApp" /apppool:"PadoruHelperBotApp AppPool"
